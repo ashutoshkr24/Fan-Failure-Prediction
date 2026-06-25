5e60ba04c68873e52490f919850b273f3e72acd8
ashutoshkr24 <ashutosh082009@gmail.com>
2026-06-25 15:03:47 +0530

    Enable CORS

--- backend/app/main.py
+++ backend/app/main.py
@@ -11,11 +11,11 @@ app = FastAPI(
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["https://fan-failure-prediction-oxrb-3hop29c25.vercel.app"],
-    allow_credentials=True,
+    allow_origins=["*"],
+    allow_credentials=False,
     allow_methods=["*"],
     allow_headers=["*"],
-)
+    )
 
 @app.on_event("startup")
 async def startup_event():
